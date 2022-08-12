c6c658ad6b4f411fb3b9b44c4869220541007789
Nikita <nikita.kras.kyiv@gmail.com>
2022-08-12 16:00:32 +0100

    Original game with bugs

--- scr/main.py
+++ scr/main.py
@@ -1,3 +1,17 @@
 import Environment
 
-Environment.create_game()
+Environment.createGame()
+
+state = Environment.initGridRand()
+
+state = Environment.makeMove(state, 1)
+print(Environment.dispGrid(state))
+state = Environment.makeMove(state, 1)
+print(Environment.dispGrid(state))
+state = Environment.makeMove(state, 1)
+print(Environment.dispGrid(state))
+state = Environment.makeMove(state, 3)
+print(Environment.dispGrid(state))
+
+print('Reward: %s' % (Environment.getReward(state),))
+print(Environment.dispGrid(state))
