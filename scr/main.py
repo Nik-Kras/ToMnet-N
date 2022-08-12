9fc720c0fbabf0d657c576df683d4515ac46c699
Nikita <nikita.kras.kyiv@gmail.com>
2022-08-12 16:30:48 +0100

    Advanced implementation with OOP

--- scr/main.py
+++ scr/main.py
@@ -1,17 +1,5 @@
 import Environment
 
-Environment.createGame()
+game = Environment.Gridworld(size = 4)
 
-state = Environment.initGridRand()
-
-state = Environment.makeMove(state, 1)
-print(Environment.dispGrid(state))
-state = Environment.makeMove(state, 1)
-print(Environment.dispGrid(state))
-state = Environment.makeMove(state, 1)
-print(Environment.dispGrid(state))
-state = Environment.makeMove(state, 3)
-print(Environment.dispGrid(state))
-
-print('Reward: %s' % (Environment.getReward(state),))
-print(Environment.dispGrid(state))
+print(game.dispGrid())
