a6339ed36af063c545b41b5adc56dc7fc34d3cf8
Nikita <nikita.kras.kyiv@gmail.com>
2022-10-08 10:40:25 +0100

    Testing accuracy still has 2 modes - around 50% and around 25%

--- scr/sandbox.py
+++ scr/sandbox.py
@@ -3,33 +3,8 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 
-# Accuracy with CNN kernel initialisation with He distribution
-# And with default Xavier distribution
-Xavier = [53 , 17 , 60 , 19,
-49 , 48 , 51 , 24,
-14 , 61 , 22 , 28 ]
-He = [54 , 50 , 56 , 21 ,
-51 , 30 , 24 , 49,
-21 , 22 , 35 , 51]
 
-data = {
-    "Xavier": Xavier,
-    "He": He
-}
+test_accuracy = [53.921568393707275, 53.7162184715271, 21.5231791138649, 21.958456933498383, 48.72611463069916, 53.45744490623474, 27.692309021949768, 25.825825333595276, 27.222222089767456, 49.70414340496063, 23.456789553165436, 25.889968872070312]
 
-data = pd.DataFrame(data) # (np.transpose(np.array([Xavier, He])))
-penguins = sns.load_dataset("penguins")
-
-# plt.hist(Xavier, bins=10)
-# plt.hist(He, bins=10)
-# plt.show()
-
-kwargs = dict(hist_kws={'alpha':.6}, kde_kws={'linewidth':2})
-
-plt.figure(figsize=(10,7), dpi= 80)
-sns.histplot(data, multiple="dodge", shrink=.8, bins=15)
-plt.xlim(10,60)
-sns.displot(data, color="dodgerblue", kind="kde")
-plt.xlim(0,80)
-plt.legend()
+plt.plot(test_accuracy)
 plt.show()
